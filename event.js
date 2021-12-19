95ec7c7dc5d06d31920806f6671cf5e6618647c0
Van Tran <tavan.it@gmail.com>
2021-12-19 14:22:43 +0100

    hide every image

--- event.js
+++ event.js
@@ -16,17 +16,23 @@
 
 // Hide Pictures
 var netherlands1 = document.getElementById("netherlands");
-var netherlandsPic = document.getElementById("netherlands-pic");
+// var netherlandsPic = document.getElementById("netherlands-pic");
 var france1 = document.getElementById("france");
-var francePic = document.getElementById("france-pic");
+// var francePic = document.getElementById("france-pic");
 var england1 = document.getElementById("england");
-var englandPic = document.getElementById("england-pic");
+// var englandPic = document.getElementById("england-pic");
 
 // same code below
 netherlands1.addEventListener("click", picLink);
 france1.addEventListener("click", picLink);
 england1.addEventListener("click", picLink);
 function picLink(){
+    // hide every image
+    var allImages = document.querySelectorAll("img");
+    for (var i = 0; i < allImages.length; i++){
+        allImages[i].className = "hide";
+    }
+
     var picId = this.attributes["data-img"].value;
     // console.log(picId);
     var pic = document.getElementById(picId);
